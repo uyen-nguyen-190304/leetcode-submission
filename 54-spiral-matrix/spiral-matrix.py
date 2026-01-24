0815fa2aab8a7c838268df8464e557cb1db9918c
Uyen Nguyen <121514283+uyen-nguyen-190304@users.noreply.github.com>
2026-01-24 12:23:29 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.24%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,46 +1,25 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # Helper function
-        def tranversal_check():
-            # Check if we have already finish the spiral tranversal 
-            if len(matrix) == 0:
-                return True
-
         # Record the numbers in spiral order
         res = []
 
-        while True:        
-            # Step 1: Append elements of the first non-empty list
-            res.extend(matrix[0])
-            matrix.pop(0)
+        while matrix:
+            # 1. Append elements of first row
+            res += matrix.pop(0)
 
-            if tranversal_check():
-                return res
+            # 2. Append last element of all rows
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
 
-            # Step 2: Append the last element of all lists in order
-            for col in matrix:
-                res.append(col[-1])
-                col.pop(-1)
-            
-            matrix = [item for item in matrix if len(item) > 0]
+            # 3. Append the last row in reverse order
+            if matrix:
+                res += (matrix.pop()[::-1])
 
-            if tranversal_check():
-                return res
+            # 4. Append the first element of all lists in reverse order
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
 
-            # Step 3: Append the reverse of the last list
-            res.extend(matrix[-1][::-1])
-            matrix.pop(-1)
-
-            if tranversal_check():
-                return res
-
-            # Step 4: Append the first element of the remaining list in reverse order
-            for col in reversed(matrix):
-                res.append(col[0])
-                col.pop(0)
-            
-            matrix = [item for item in matrix if len(item) > 0]
-
-            if tranversal_check():
-                return res
-    
+        # Return such order
+        return res
