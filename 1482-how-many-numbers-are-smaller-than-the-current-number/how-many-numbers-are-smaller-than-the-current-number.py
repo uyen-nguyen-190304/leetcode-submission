8f9f7179320a510c977238b07455582c748c43cf
Uyen Nguyen <121514283+uyen-nguyen-190304@users.noreply.github.com>
2026-01-24 10:08:48 -0500

    Time: 4 ms (62.95%) | Memory: 19.2 MB (27.12%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,12 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        # Array to hold the return answer
         ret = []
 
-        for _, v in enumerate(nums):
-            counter = 0
-            for _, k in enumerate(nums):
-                if k < v:
-                    counter += 1
-            ret.append(counter)
+        # Sort the array in ascending order
+        nums_sorted = sorted(nums)
 
+        # Loop through the sorted version to count smaller number than current
+        hash_map = {}
+        for i, v in enumerate(nums_sorted):
+            if v not in hash_map:
+                hash_map[v] = i
+
+        # Translate that to the original array
+        for _, k in enumerate(nums):
+            ret.append(hash_map[k])
+
+        # Return such answer
         return ret
