a3258160720c7c6c02afcc5886b928cf4f8bc482
Uyen Nguyen <121514283+uyen-nguyen-190304@users.noreply.github.com>
2026-01-25 07:15:38 -0500

    Time: 63 ms (59.67%) | Memory: 28.8 MB (15.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,25 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Number of days n 
-        n = len(prices)
+        max_profit = 0
+        buy, sell  = 0, 1
 
-        # Create a DP array OPT
-        # OPT[i] = best that we can do till day i
-        OPT = [0] * (n + 1)
-        
-        # Base case: Day 0 -> nothing so far
-        OPT[0] = 0
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                profit = prices[sell] - prices[buy]
+                if profit > max_profit:
+                    max_profit = profit
+            else:
+                buy = sell
+            sell = sell + 1
 
-        min_price_so_far = prices[0]
-        # Recursive formula: either do nothing or sell (if already buy)
-        for i in range(1, n + 1):
-            # Fill the DP array
-            price_today = prices[i - 1]
-            OPT[i] = max(OPT[i - 1], price_today - min_price_so_far)
-
-            # Update the min_prices_so_far if applicable
-            if price_today < min_price_so_far:
-                min_price_so_far = price_today
-
-        # Return the best possible profit
-        return OPT[n]
+        return max_profit
